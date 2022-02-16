c6c8b53d885d42595d5f94293fa279fd2fc5b16c
Petrik Tanár <info@petrik.hu>
2022-02-16 12:20:38 +0100

    Add project files.

---
+++ GitVisualStudioDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitVisualStudioDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
